f5b69d2c170565d341de31588ef4051a95afb215
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-03 12:14:40 -0300

    disposal form new url

--- server/utils/urls.js
+++ server/utils/urls.js
@@ -1,4 +1,4 @@
-const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbyH1jWlisJCkMnynegpE37t0VYynk-dC4NkI9D52q6TjSKNCtCfLcKLbCKmc_9ETV_g/exec'
+const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbzPeeBbn1K74QpbqXfQpCD2VwdINCqkAmQXD6uP3f0kI9T0kieLgICep-zJYfafNM4B/exec'
 const googleSheetsUrlPrinters = 'https://script.google.com/macros/s/AKfycbyYUpdUlfK48Cbt_SH--JkSwlobha-xHZJN3utCgGIsAdOF-4FAgtG141X-z6X_YoH_/exec'
 
 export default {
