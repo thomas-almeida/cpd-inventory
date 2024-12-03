006de4bfabec4197e2855bfb919db00c76d940ff
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-03 09:51:11 -0300

    debug

--- server/controllers/disposalController.js
+++ server/controllers/disposalController.js
@@ -11,10 +11,12 @@ async function sendForm(req, res) {
     })
 
     // Retorna a resposta do Google Apps Script para o frontend
+    console.log(response.data)
     res.status(200)(response.data)
   } catch (error) {
     console.error('Erro ao enviar para o Google Apps Script:', error);
     res.status(500).json({ status: 'error', message: 'Erro ao enviar o formulário.' });
+    console.log(error)
   }
 }
 
