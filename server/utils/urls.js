2b40c8bf38b8ea1487eed8df82bb8a1cedc582f2
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-03 12:24:24 -0300

    update url disposal

--- server/utils/urls.js
+++ server/utils/urls.js
@@ -1,4 +1,4 @@
-const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbzPeeBbn1K74QpbqXfQpCD2VwdINCqkAmQXD6uP3f0kI9T0kieLgICep-zJYfafNM4B/exec'
+const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbxMkKw2pxf7JxZDgeTbmyXXN6nU4xAxa-39J03MPcuzg3dNCFhqxSzhHvitU7Ide_qi/exec'
 const googleSheetsUrlPrinters = 'https://script.google.com/macros/s/AKfycbyYUpdUlfK48Cbt_SH--JkSwlobha-xHZJN3utCgGIsAdOF-4FAgtG141X-z6X_YoH_/exec'
 
 export default {
