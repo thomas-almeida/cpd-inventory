62cfee87d0ca6b7219d62c0e2544de0ce2e8da06
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-03 16:03:07 -0300

    new url

--- server/utils/urls.js
+++ server/utils/urls.js
@@ -1,4 +1,4 @@
-const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycby47cDg5TZXpQ5-UIl5o9lEduWSeVngSp2yzncr3NTN4C29r0dp0tMEBiMV0GCg4lji/exec'
+const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbwA6OXePxkCINGosAS0wR4Gdgwx_SzrV2pHBgW2rW1wiOgzWGJYtFsX4ynkgIm0teo/exec'
 const googleSheetsUrlPrinters = 'https://script.google.com/macros/s/AKfycbyYUpdUlfK48Cbt_SH--JkSwlobha-xHZJN3utCgGIsAdOF-4FAgtG141X-z6X_YoH_/exec'
 
 export default {
